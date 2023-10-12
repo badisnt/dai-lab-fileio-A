768ac65b9898734e96f057ede4a15244dcf91bc1
kdemont <kilian.demont@heig-vd.ch>
2023-10-12 19:08:53 +0200

    Revert "Encoding Selector for kdemont"
    This reverts commit 8b5ca4b5da7980a4b16f5a9e5beeb55622461cb9.

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/kdemont/FileExplorer.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/kdemont/FileExplorer.java
@@ -10,7 +10,6 @@ public class FileExplorer {
     /**
      * Constructor
      * Memorize the folder to explore and initialize the set of known files.
-     *
      * @param folder
      */
     public FileExplorer(String folder) {
@@ -23,21 +22,10 @@ public class FileExplorer {
      * The file must not have been returned before.
      * Use the java.io.file API to get the list of files in the folder.
      * Use the HashSet knownFiles to keep track of the files that have already been returned.
-     *
      * @return a new file, or null if there is no new file
      */
     public File getNewFile() {
-        File[] files = folder.listFiles();
-
-        assert files != null;
-
-        // find the first file in folder that is not in known files
-        for (File file : files) {
-            if (!knownFiles.contains(file)) {
-                knownFiles.add(file);
-                return file;
-            }
-        }
+        // TODO: implement the method body here
         return null;
     }
 }
