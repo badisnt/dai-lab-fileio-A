e5e58f2e38ca82eb530ac0483e0c4af94dc64a69
Rafou98 <95037521+Rafou28@users.noreply.github.com>
2023-10-12 17:09:21 +0200

    FileReaderWriter done by Rafou2898

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Rafou2898/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Rafou2898/FileReaderWriter.java
@@ -7,7 +7,8 @@ public class FileReaderWriter {
 
     /**
      * Read the content of a file with a given encoding.
-     * @param file the file to read. 
+     *
+     * @param file     the file to read.
      * @param encoding
      * @return the content of the file as a String, or null if an error occurred.
      */
@@ -15,13 +16,30 @@ public class FileReaderWriter {
         // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to close the streams and readers at the end.
-        return null;
+        StringBuilder content = new StringBuilder();
+        try (FileInputStream fileReader = new FileInputStream(file)) {
+
+            String line;
+            var reader = new BufferedReader(new InputStreamReader(fileReader, encoding));
+
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\r\n");
+            }
+
+            reader.close();
+            return content.toString();
+
+        } catch (IOException e) {
+            System.out.println("Exception: " + e);
+            return null;
+        }
     }
 
     /**
-     * Write the content to a file with a given encoding. 
-     * @param file the file to write to
-     * @param content the content to write
+     * Write the content to a file with a given encoding.
+     *
+     * @param file     the file to write to
+     * @param content  the content to write
      * @param encoding the encoding to use
      * @return true if the file was written successfully, false otherwise
      */
@@ -29,6 +47,17 @@ public class FileReaderWriter {
         // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+
+        try(var myfileOutputStream = new FileOutputStream(file)){
+            var writer = new BufferedWriter(new OutputStreamWriter (myfileOutputStream, encoding));
+            writer.write(content);
+            writer.flush();
+            writer.close();
+            return true;
+        } catch(IOException e){
+            System.out.println("Exception: " + e);
+            return false;
+        }
+
     }
 }
