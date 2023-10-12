e15b49daa8099503f965c2af77e74e393315109e
kdemont <kilian.demont@heig-vd.ch>
2023-10-12 19:10:28 +0200

    Encoding Selector for kdemont

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/kdemont/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/kdemont/EncodingSelector.java
@@ -5,6 +5,8 @@ import java.nio.charset.Charset;
 
 public class EncodingSelector {
 
+
+
     /**
      * Get the encoding of a file based on its extension.
      * The following extensions are recognized:
@@ -12,12 +14,36 @@ public class EncodingSelector {
      *   - .txt:     US-ASCII
      *   - .utf16be: UTF-16BE
      *   - .utf16le: UTF-16LE
-     * 
+     *
      * @param file the file to get the encoding from
      * @return the encoding of the file, or null if the extension is not recognized
      */
     public Charset getEncoding(File file) {
-        // TODO: implement the method body here
-        return null;
+        String fileName = file.getName();
+        //  Get the file extension
+        String fileExtension = getFileExtension(fileName);
+
+        // Check the file extension and return the corresponding encoding
+        if (fileExtension.equalsIgnoreCase("utf8")) {
+            return Charset.forName("UTF-8");
+        } else if (fileExtension.equalsIgnoreCase("txt")) {
+            return Charset.forName("US-ASCII");
+        } else if (fileExtension.equalsIgnoreCase("utf16be")) {
+            return Charset.forName("UTF-16BE");
+        } else if (fileExtension.equalsIgnoreCase("utf16le")) {
+            return Charset.forName("UTF-16LE");
+        } else {
+            // If the extension is not recognised, return null
+            return null;
+        }
+    }
+
+    private String getFileExtension(String fileName) {
+        int lastDotIndex = fileName.lastIndexOf('.');
+        if (lastDotIndex != -1) {
+            return fileName.substring(lastDotIndex + 1);
+        } else {
+            return ""; // If no extension is found, return an empty string
+        }
     }
 }
